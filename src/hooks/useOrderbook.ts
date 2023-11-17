774897344f35be06b3eb1ab76ac8c8b1a9a04df9
nenadV91 <nenadwebdevelop@gmail.com>
2023-11-17 18:47:57 +0100

    fix: clean up useOrderbook hook

--- src/hooks/useOrderbook.ts
+++ src/hooks/useOrderbook.ts
@@ -1,47 +1,15 @@
 import { useEffect, useState, useCallback } from "react";
+import { Subscription } from "centrifuge";
 import { useCentrifuge } from "./useCentrifuge";
-import { BidAskType } from "../types";
 import { updateOrderBookItems } from "../utils/updateOrderBookItems";
-import {
-  PublicationContext,
-  SubscribedContext,
-  Subscription,
-} from "centrifuge";
+import { BidAskType } from "../types";
 
 export const useOrderbook = (market: string) => {
   const { centrifuge } = useCentrifuge();
 
   const [bids, setBids] = useState<BidAskType>([]);
   const [asks, setAsks] = useState<BidAskType>([]);
-  const [lastSequence, setLastSequence] = useState<number | null>(null);
   const [sub, setSub] = useState<Subscription | null>(null);
-
-  const handlePublication = useCallback(
-    (ctx: PublicationContext) => {
-      if (!sub) return;
-      // if (lastSequence && lastSequence + 1 !== ctx.data.lastSequence) {
-      //   sub.unsubscribe();
-      //   sub.removeAllListeners();
-      //   setSub(null);
-      // } else {
-      //   setBids((bids) => updateOrderBookItems(bids, ctx.data.bids));
-      //   setAsks((asks) => updateOrderBookItems(asks, ctx.data.asks));
-      //   setLastSequence(ctx.data?.sequence);
-      // }
-
-      setBids((bids) => updateOrderBookItems(bids, ctx.data.bids));
-      setAsks((asks) => updateOrderBookItems(asks, ctx.data.asks));
-      setLastSequence(ctx.data?.sequence);
-    },
-    [sub]
-  );
-
-  const handleSubscription = useCallback((ctx: SubscribedContext) => {
-    console.log("Subscribed", ctx);
-    setBids(updateOrderBookItems(ctx.data?.bids, null));
-    setAsks(updateOrderBookItems(ctx.data?.asks, null));
-    setLastSequence(ctx.data.sequence);
-  }, []);
 
   const subscribe = useCallback(() => {
     if (!centrifuge) return;
@@ -50,14 +18,21 @@ export const useOrderbook = (market: string) => {
       setSub(centrifuge.newSubscription(`orderbook:${market}`));
     } else {
       sub
-        .on("publication", handlePublication)
-        .on("subscribed", handleSubscription)
-        .on("unsubscribed", function (ctx) {
+        .on("publication", (ctx) => {
+          setBids((bids) => updateOrderBookItems(bids, ctx.data.bids));
+          setAsks((asks) => updateOrderBookItems(asks, ctx.data.asks));
+        })
+        .on("subscribed", (ctx) => {
+          console.log("Subscribed", ctx);
+          setBids(updateOrderBookItems(ctx.data?.bids, null));
+          setAsks(updateOrderBookItems(ctx.data?.asks, null));
+        })
+        .on("unsubscribed", (ctx) => {
           console.log(`unsubscribed: ${ctx.code}, ${ctx.reason}`);
         })
         .subscribe();
     }
-  }, [centrifuge, handlePublication, handleSubscription, market, sub]);
+  }, [centrifuge, market, sub]);
 
   useEffect(() => {
     subscribe();
